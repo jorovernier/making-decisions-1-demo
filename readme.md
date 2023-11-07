341b37a5b01aef2feb04542a4fd75ccedca337ef
jorovernier <jorovernier@gmail.com>
2023-11-07 09:47:20 -0700

    added annotations

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# Making Decisions 2 Demo
+# Making Decisions 1 Demo
 
-This is a finished version of the Making Decisions 2 demo. Use this for all your referencing needs!
+This is a finished version of the Making Decisions 1 demo. Use this for all your referencing needs!
--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -12,7 +12,7 @@ if(pikachuAttack > onixAttack){
 } else if(onixAttack > pikachuAttack){
     console.log('Onix is obviously the stronger pokemon.')
 } else {
-    console.log("Wow, they're evenely matched!")
+    console.log("Wow, they're evenly matched!")
 }
 
 let pikachuHealth = 100;
@@ -21,14 +21,17 @@ let pikachuDefense = 0;
 if(pikachuHealth <= onixAttack){
     console.log('Pikachu has fainted!')
 } else {
+    // These do the same thing, the second way is just quicker to type.
+
+    // Long Way
     // pikachuHealth = pikachuHealth - onixAttack
+
+    // Shorthand
     pikachuHealth -= onixAttack
     console.log(`Pikachu's health is down to ${pikachuHealth}.`)
 }
 
 pikachuDefense += 25;
-
-// pikachuHealth = 10;
 
 if(pikachuHealth <= onixAttack - pikachuDefense){
     console.log('Pikachu has fainted!')
@@ -43,28 +46,38 @@ if((pikachuHealth + 50) >= 100){
     pikachuHealth += 50
 }
 
+console.log(`Pikachu was healed, his health is now ${pikachuHealth}.`)
+
+// These if statments do the same thing, just with slightly different logic.
 let ironTailHits = true;
 
+// With ===
 if(ironTailHits === true){
     console.log('The battle continues!')
 } else {
     console.log('Pikachu flees the battle!')
 }
 
-// for(let i = 0; i < 5; i++){
-//     pikachuHealth -= (onixAttack - pikachuDefense)
-//     console.log(`Pikachu's health is down to ${pikachuHealth}.`)
-// }
+// With !==
+if(ironTailHits !== false){
+    console.log('The battle continues!')
+} else {
+    console.log('Onix fled the battle!')
+}
 
-// for(let i = 0; i < 5; i++){
-//     if(pikachuHealth <= 0){
-//         console.log('Pikachu has fainted!')
-//     } else {
-//         pikachuHealth -= (onixAttack - pikachuDefense)
-//         console.log(`Pikachu's health is down to ${pikachuHealth}.`)
-//     }
-// }
+// These loops so the same thing, just with different syntax.
 
+// For Loop
+for(let i = 0; i < 5; i++){
+    if(pikachuHealth <= 0){
+        console.log('Pikachu has fainted!')
+    } else {
+        pikachuHealth -= (onixAttack - pikachuDefense)
+        console.log(`Pikachu's health is down to ${pikachuHealth}.`)
+    }
+}
+
+// While Loop
 while(pikachuHealth > 0){
     pikachuHealth -= (onixAttack - pikachuDefense)
     console.log(`Pikachu's health is down to ${pikachuHealth}.`)
